6e161ef78512b76adc03f23c29abc432405c4977
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-02-15 00:22:11 +0600

    game developed

--- scripts.js/aplha-clash.js
+++ scripts.js/aplha-clash.js
@@ -10,10 +10,75 @@
 //     //step- 2 show the play ground
 // }
 
+function handleKeyboardKeuUpEvent(event) {
+    const playerPressed = event.key;
+
+    //stop the game if pressed Esc
+    if (playerPressed === 'Escape') {
+        gameOver();
+    }
+    //get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+    // check matched or not
+    if (playerPressed === expectedAlphabet) {
+        console.log('You got a point!');
+        const currentScore = getTextElementValueById('current-score');
+        const newScore = currentScore + 1;
+
+        setTextElementValueById('current-score', newScore);
+
+
+        //update score---------------------without func-----------------------------
+        //1. get the current score
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+
+        //2. increase the score by 1
+        // const newScore = currentScore + 1;
+        //3. show the updated score
+        // currentScoreElement.innerText = newScore;
+
+        // start a new round
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else {
+        console.log('you missed');
+        const currentLife = getTextElementValueById('current-life');
+        const newLife = currentLife - 1;
+        setTextElementValueById('current-life', newLife)
+
+
+        //---------------------------------------------
+        //without func
+        //1. get the current life
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+
+        //2. decrease the life count
+        // const newLife = currentLife - 1;
+
+        //3. show the remaining life
+        // currentLifeElement.innerText = newLife;
+
+        if (newLife === 0) {
+            gameOver();
+        }
+    }
+}
+// capture keyboard key press
+document.addEventListener('keyup', handleKeyboardKeuUpEvent);
+
 function continueGame() {
     //step-1: generate a random alphabet
     const alphabet = getARandomAlphabet();
-    console.log(alphabet);
+    // console.log(alphabet);
 
     //set randomly 
     const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -24,7 +89,27 @@ function continueGame() {
 }
 
 function play() {
+    //hide everything and show only the play ground
     hideElementById('home-screen');
+    hideElementById('final-score')
     showElementById('play-ground');
+    // reset life
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
     continueGame();
+}
+
+function gameOver() {
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    //update final score
+    //1.get the score
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueById('game-score', lastScore);
+
+    //  clear the last selected alphabet highlight
+    const currentAlphabet = getElementTextValueById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
 }
--- scripts.js/utility.js
+++ scripts.js/utility.js
@@ -18,6 +18,23 @@ function removeBackgroundColorById(elementID) {
     element.classList.remove('bg-amber-400');
 }
 
+function getTextElementValueById(elementID) {
+    const element = document.getElementById(elementID);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+function getElementTextValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
 function getARandomAlphabet() {
     //get or create an alphabet array
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
